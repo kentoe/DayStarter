25e968d328f0e5c2dc58f074406a3b1a6b6791c6
Ryan Warner <kentoe@gmail.com>
2013-10-27 19:32:20 -0400

    Added more to background service framework

--- src/com/warner/daystarter/services/BackgroundNoiseService.java
+++ src/com/warner/daystarter/services/BackgroundNoiseService.java
@@ -3,10 +3,14 @@ package com.warner.daystarter.services;
 import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
+
+import com.warner.daystarter.R;
 
 public class BackgroundNoiseService extends Service
 {
+	MediaPlayer player;
 
 	@Override
 	public IBinder onBind(Intent intent)
@@ -14,6 +18,52 @@ public class BackgroundNoiseService extends Service
 		// TODO Auto-generated method stub
 		return null;
 	}
+	@Override
+    public void onCreate() 
+	{
+        super.onCreate();
+        player = MediaPlayer.create(this, R.raw.waves);
+        player.setLooping(true); // Set looping
+        player.setVolume(100,100);
 
+    }
+	
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        player.start();
+        return 1;
+    }
+
+    public void onStart(Intent intent, int startId) 
+    {
+        // TO DO
+    }
+    
+    public IBinder onUnBind(Intent arg0) 
+    {
+        // TO DO Auto-generated method
+        return null;
+    }
+
+    public void onStop() 
+    {
+
+    }
+    
+    public void onPause() 
+    {
+
+    }
+    
+    @Override
+    public void onDestroy() {
+        player.stop();
+        player.release();
+    }
+
+    @Override
+    public void onLowMemory() {
+
+    }
 
 }
