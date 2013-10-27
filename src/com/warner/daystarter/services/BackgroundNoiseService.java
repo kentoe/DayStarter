599b2da0630dd6ae3e48ca14d9f0b8c0079148a9
Ryan Warner <kentoe@gmail.com>
2013-10-27 19:09:58 -0400

    Added framework for future background service

---
+++ src/com/warner/daystarter/services/BackgroundNoiseService.java
@@ -0,0 +1,19 @@
+package com.warner.daystarter.services;
+
+import android.app.IntentService;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BackgroundNoiseService extends Service
+{
+
+	@Override
+	public IBinder onBind(Intent intent)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
